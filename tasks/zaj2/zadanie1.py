c7f386517f11fb7cf69bdd9654c29b2262b85ad5
Konrad Wisniewski <konwis@student.if.pw.edu.pl>
2014-10-30 15:05:59 +0100

    cokolwiek

--- tasks/zaj2/zadanie1.py
+++ tasks/zaj2/zadanie1.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
-
-
 def xrange(start_stop, stop=None, step=None):
-    """
-    Funkcja która działa jak funkcja range (wbudowana i z poprzednich zajęć)
-    która działa dla liczb całkowitych.
-    """
+	i=0
+	stop2=start_stop
+	if stop==None:		
+		start_stop=0
+		stop=stop2		
+	if step==None:
+		step=1
+	while(i<((stop-start_stop)/step)):
+		yield (start_stop+i*step)
+		i+=1
+xrange(-3,10,2)
+
+#    """
+#    Funkcja która działa jak funkcja range (wbudowana i z poprzednich #zajęć)
+#    która działa dla liczb całkowitych.
+#    """
--- tasks/zaj2/zadanie3.py
+++ tasks/zaj2/zadanie3.py
@@ -5,65 +5,26 @@ import math
 
 class Integrator(object):
 
-    """
-    Klasa która implementuje całki metodą Newtona Cotesa z użyciem interpolacji
-    N-tego stopnia :math:`n\in<2, 11>`.
-
-    .. note::
-
-        Używamy wzorów NC nie dlatego że są super przydatne (zresztą gorąco
-        zniechęcam Państwa przed pisaniem własnych podstawowych algorytmów
-        numerycznych --- zbyt łatwo o głupi błąd) ale dlatego żebyście
-        jescze raz napisali jakiś algorytm w którym nie opłaca się zrobić 11
-        ifów.
-
-    """
-
     @classmethod
     def get_level_parameters(cls, level):
-        """
-
-        :param int level: Liczba całkowita większa od jendości.
-        :return: Zwraca listę współczynników dla poszczególnych puktów
-                 w metodzie NC. Na przykład metoda NC stopnia 2 używa punktów
-                 na początku i końcu przedziału i każdy ma współczynnik 1,
-                 więc metoda ta zwraca [1, 1]. Dla NC 3 stopnia będzie to
-                 [1, 3, 1] itp.
-        :rtype: List of integers
-        """
+       
+        param = [[1/2, 1/2],[1/3, 4/3, 1/3],[3/8, 9/8, 9/8, 3/8],[14/45, 64/45, 24/45, 64/45, 14/45]]
+        return param[level]
 
     def __init__(self, level):
-        """
-        Funkcja ta inicjalizuje obiekt do działania dla danego stopnia metody NC
-        Jeśli obiekt zostanie skonstruowany z parametrem 2 używa metody trapezów.
-        :param level: Stopień metody NC
-        """
+
         self.level = level
 
     def integrate(self, func, func_range, num_evaluations):
-        """
-        Funkcja dokonuje całkowania metodą NC.
+        value = 0
+        h = (func_range[1]-func_range[0])/(num_evaluations//self.level)
+        dx = h / (self.level-1)
+        param = self.get_level_parameters(self.level-2)       
+        for x in range(num_evaluations//self.level):
+            for y in range(self.level):
+                value += func(y*dx + x*h + func_range[0])*param[y]*dx
+        return value
 
-        :param callable func: Całkowana funkcja, funkcja ta ma jeden argument,
-                              i jest wołana w taki sposób: `func(1.0)`.
-        :param Tuple[int] func_range: Dwuelementowa krotka zawiera początek i koniec
-                                 przedziału całkowania.
-        :param int num_evaluations: Przybliżona lość wywołań funkcji ``func``,
-            generalnie algorytm jest taki:
-
-            1. Dzielimy zakres na ``num_evaluations/self.level`` przdziałów.
-               Jeśli wyrażenie nie dzieli się bez reszty, należy wziąć najmiejszą
-               liczbę całkowitą większą od `num_evaluations/self.level``. 
-            2. Na każdym uruchamiamy metodę NC stopnia ``self.level``
-            3. Wyniki sumujemy.
-
-            W tym algorytmie wykonamy trochę więcej wywołań funkcji niż ``num_evaluations``,
-            dokłanie ``num_evaluations`` byłoby wykonywane gdyby keszować wartości
-            funkcji na brzegach przedziału całkowania poszczególnych przedziałów.
-
-        :return: Wynik całkowania.
-        :rtype: float
-        """
 
 
 if __name__ == '__main__':
