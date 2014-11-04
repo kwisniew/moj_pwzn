4fc198137e563e9cf3f4f920b6b4d034487d66ba
unknown <kon.wu888@gmail.com>
2014-11-04 11:33:23 +0100

    zadanie 2 zrobione

--- tasks/zaj2/zadanie2.py
+++ tasks/zaj2/zadanie2.py
@@ -20,77 +20,62 @@ def filter_animals(animal_list):
     f=None
     genus = None
     noe = []
+    animal_list_tmp = []
+    mass_list = []
     
-    print(len(animal_list))
-    animal_list.sort(key=itemgetter('genus', 'name'))
-    print(len(animal_list))
+
+            
+    animal_list.sort(key=itemgetter('genus'))
+    
+    for idx, d in enumerate(animal_list): 
+        mass, unit = d['mass']
+        if unit == 'kg':
+            mass_list.append(mass*1000)
+        if unit == 'g':
+            mass_list.append(mass/1000)
+        if unit == 'mg':
+            mass_list.append(mass/1000000)
+        if unit == 'Mg':
+            mass_list.append(mass*1000)    
+
+    
+    f_idx=None
+    m_idx=None
     genus = animal_list[0]['genus']
-    for ii in animal_list:       
+    for idx, ii in enumerate(animal_list):       
         if genus==ii['genus']:
             if ii['sex']=='male':
                 if m==None:
                     m=ii
-                elif m['mass'] > ii['mass']:
+                    m_idx=idx   
+                elif mass_list[idx]<mass_list[m_idx]:
                     m=ii
+                    m_idx=idx
             elif ii['sex']=='female':
                 if f==None:
                     f=ii
-                elif f['mass'] > ii['mass']:
+                    f_idx=idx
+                elif mass_list[idx]<mass_list[f_idx]:
                     f=ii
-        else:           
-            noe.append(f)
+                    f_idx=idx
+        else:
             noe.append(m)
+            noe.append(f)
             genus=ii['genus']
             if ii['sex']=='male':
                 m=ii
+                m_idx=idx
                 f=None
             else:
                 m=None
                 f=ii
+                f_idx=idx
     noe.append(f)
     noe.append(m)
                 
              
-    print(len(noe))   
+    noe.sort(key=itemgetter('genus', 'name'))
     return noe
-    #print(noe)
-    #noe.sort(key=itemgetter('genus', 'name'))
-    #for i in noe[0:100]:
-    #    print(i)
-    
-    """
-    
 
-    Jesteś informatykiem w firmie Noe Shipping And Handling. Firma ta zajmuje
-    się międzykontynentalnym przewozem zwierząt.
-
-    Dostałeś listę zwierząt które są dostępne w pobliskim zoo do transportu.
-
-    Mususz z tej listy wybrać listę zwierząt które zostaną spakowane na statek,
-
-    Lista ta musi spełniać następujące warunki:
-
-    * Docelowa lista zawiera obiekty reprezentujące zwierzęta (tak jak animal_list)
-    * Z każdego gatunku zwierząt (z tej listy) musisz wybrać dokładnie dwa
-      egzemplarze.
-    * Jeden egzemplarz musi być samicą a drugi samcem.
-    * Spośród samic i samców wybierasz te o najmniejszej masie.
-    * Dane w liście są posortowane względem gatunku a następnie nazwy zwierzęcia
-
-    Wymaganie dla osób aspirujących na ocenę 5:
-
-    * Ilość pamięci zajmowanej przez program musi być stała względem
-      ilości elementów w liście zwierząt.
-    * Ilość pamięci może rosnąć liniowo z ilością gatunków.
-
-    Nie podaje schematu obiektów w tej liście, musicie radzić sobie sami
-    (można podejrzeć zawartość listy w interaktywnej sesji interpretera).
-
-    Do załadowania danych z listy możesz użyć metody `load_animals`.
-
-    :param animal_list:
-    """
-
-#if __name__ == "__main__":
-animals = load_animals()
+animals = load_animals(True)
 filter_animals(animals)
